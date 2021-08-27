0f6149b93b225bdbb1a3b3752a490b51f46e393e
Nagendra Kumar <47654016+dvsnkumar@users.noreply.github.com>
2021-08-27 08:39:53 +0530

    Update SecurityUtil.java

--- notofication-module-master/notification-service-common/src/main/java/com/personal/notification/core/util/SecurityUtil.java
+++ notofication-module-master/notification-service-common/src/main/java/com/personal/notification/core/util/SecurityUtil.java
@@ -8,7 +8,7 @@ import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
-import javax.xml.bind.DatatypeConverter;
+//import javax.xml.bind.DatatypeConverter;
   
 /**
  * 
